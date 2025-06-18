32ad97da977cdf895a641e22205ca912c939a39d
Thuy Le <ngocthuyle19@gmail.com>
2025-06-18 07:22:47 -0400

    add user in database by using prisma

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await currentUser();
+  if (user) {
+    const a = await syncUser()
+    console.log('sync user', a)};
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16">
-            <div className="flex items-center">
-                <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
-                Socially
-                </Link>
-            </div>
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider">
+              Socially
+            </Link>
+          </div>
 
-            <DesktopNavbar />
-            <MobileNavbar />
-
+          <DesktopNavbar />
+          <MobileNavbar />
         </div>
-
       </div>
       Navbar
     </nav>
