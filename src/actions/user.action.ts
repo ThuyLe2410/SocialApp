65dc9bb956b38f8a59566031816fdb368939f31a
Thuy Le <ngocthuyle19@gmail.com>
2025-06-19 11:23:03 -0400

    getUserData

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,11 +1,12 @@
 "use server"
 import prisma from "@/lib/prisma"
 import {auth, currentUser} from "@clerk/nextjs/server"
+
 export async function syncUser() {
     try {
         const {userId} = await auth()
         const user = await currentUser()
-        
+
         if (!userId || !user) return
         const existingUser = await prisma.user.findUnique({
             where: {
@@ -27,4 +28,30 @@ export async function syncUser() {
     } catch(error){
         console.log("Error in syncUser", error)
     }
+}
+
+export async function getUserByClerkId(clerkId:string) {
+    return prisma.user.findUnique({
+        where:{
+            clerkId
+        },
+        include:{
+            _count: {
+                select:{
+                    followers:true,
+                    following:true,
+                    posts:true
+                }
+            }
+        }
+    })
+
+}
+
+export async function getDbUserId() {
+    const {userId:clerkId} = await auth();
+    if (!clerkId) throw new Error("Unauthorized");
+    const user = await getUserByClerkId(clerkId);
+    if (!user) throw new Error("User not found")
+    return user.id
 }
