4a3eb72db54660841dc899aee81bbe11932077cb
Thuy Le <ngocthuyle19@gmail.com>
2025-06-20 12:25:48 -0400

    who to follow

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Loader2Icon } from "lucide-react";
+import toast from "react-hot-toast";
+import { toggleFollow } from "@/actions/user.action";
+
+export default function FollowButton({ userId }: { userId: string }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleFollow = async () => {
+    setIsLoading(true);
+    try {
+      await toggleFollow(userId);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error to following a user");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Button
+      size={"sm"}
+      variant={"secondary"}
+      onClick={handleFollow}
+      disabled={isLoading}
+      className="w-20">
+      {isLoading ? <Loader2Icon className="size-4 animate-spin" /> : "Follow"}
+    </Button>
+  );
+}
